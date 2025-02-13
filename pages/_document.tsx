3717b13494119a1bf608b4e001cda47249429832
H Joseph <hjoseph777@gmail.com>
2025-02-13 08:07:27 -0500

    main

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="en" className="dark">
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              try {
+                if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                  document.documentElement.classList.add('dark')
+                }
+              } catch (_) {}
+            `,
+          }}
+        />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
